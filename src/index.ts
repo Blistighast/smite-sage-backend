e0c98f6fa23ac6530e8fdef3d67feb68691c4383
Brian Guterl <brianguterl99@gmail.com>
2023-04-10 20:28:44 -0400

    forgot to save index, added in index changed

--- src/index.ts
+++ src/index.ts
@@ -54,7 +54,29 @@ setInterval(async () => {
     patchData.version_string
   );
   if (patchData.version_string !== patchNumber) {
-    console.log("something changed");
+    console.log("getting gods");
+    const timeStamp = DateTime.utc().toFormat("yyyyMMddHHmmss");
+    const signature = createSignature("getgods", timeStamp);
+    const getGodsUrl = `${apiUrl}/getgodsjson/${devId}/${signature}/${session}/${timeStamp}/1`;
+    const smiteResp = await fetch(getGodsUrl);
+    const godsData = await smiteResp.json();
+    godsData.forEach(async (god) => {
+      GodModel.updateOne({ id: god.id }, god, { upsert: true }, function (err) {
+        if (err) console.error(err);
+      });
+      const signature = createSignature("getgodskins", timeStamp);
+      const getGodSkinsUrl = `${apiUrl}/getgodskinsjson/${devId}/${signature}/${session}/${timeStamp}/${god.id}/1`;
+      const smiteResp = await fetch(getGodSkinsUrl);
+      const skinsData = await smiteResp.json();
+      GodModel.updateOne(
+        { id: god.id },
+        { skins: skinsData },
+        { upsert: true },
+        function (err) {
+          if (err) console.error(err);
+        }
+      );
+    });
   }
   patchNumber = patchData.version_string;
 }, 1000 * 60 * 60 * 24);
