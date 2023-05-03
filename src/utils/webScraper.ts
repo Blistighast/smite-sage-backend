a220ea7d6b0d01020bc78fd5dda8dae1b0691435
Brian Guterl <brianguterl99@gmail.com>
2023-05-03 19:25:59 -0400

    removed cheerio, added puppeteer, got webScraper working

--- src/utils/webScraper.ts
+++ src/utils/webScraper.ts
@@ -1,52 +1,37 @@
-import { load } from "cheerio";
-import fetch from "node-fetch";
+import puppeteer from "puppeteer";
 
 const webScraper = async () => {
-  console.log("scraped a website");
-  // const url = "https://www.theguardian.com/uk";
   const url = "https://www.smitegame.com/news/";
-  // const url = "http://localhost:3000/gods";
+
+  console.log(`scraping ${url}`);
+  // { headless: false } use in launch to see browser for testing
+  const browser = await puppeteer.launch({ headless: false });
   try {
-    const res = await fetch(url);
-    const html = await res.text();
-    console.log(html);
-    const $ = load(html);
-    const articles = [];
-    // console.log($(`.posts-wrapper`, html));
-    // $(`.fc-item__link`, html).each((i, ele) => {
-    // $(`.home-tile`, html).each((i, ele) => {
-    // const $a = $(".posts-wrapper").find('a').each((i, ele) => {
-    //   console.log(ele)
-    // });
-    // const links = [...$("a")].map((e) => $(e).attr("href"));
-    // console.log(links);
-    // $(".posts-wrapper")
-    //   .find("a")
-    //   .each((i, ele) => {
-    //     console.log(ele);
-    //   });
-    // console.log("ele", $a);
-    // $(`.posts-wrapper a`, html).each((i, ele) => {
-    //   console.log("index", i);
-    //   console.log("ele", ele);
-    //   const title = $(ele).text();
-    //   const link = $(ele).find("a").attr("href");
-    //   console.log(link);
-    //   const article = {};
-    // });
-    return html;
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.waitForSelector(".home-tile");
+    // await page.waitForSelector('.headline')
+    // await page.waitForSelector('.subhead-right')
+    let godTiles = await page.evaluate(() => {
+      let tileElements = document.body.querySelectorAll(".posts-wrapper a");
+      let godTiles = Object.values(tileElements).map((tile) => {
+        // if ()
+        return {
+          tileUrl: tile.getAttribute("href"),
+          headLine: tile.querySelector(".headline").textContent ?? null,
+          // subhead: tile.querySelector(".subhead-right div").contains("div")
+        };
+      });
+      return godTiles;
+    });
+    await browser.close();
+    console.log(godTiles);
+    return godTiles;
   } catch (err) {
     console.error(err);
+    console.log("scrape failed");
+    await browser.close();
   }
-  // console.log(articles);
-  // const god = { link: null };
-  // $(`.card_godCard__kb9Ew`, html).each((i, ele) => {
-  //   const list = $(ele).text();
-  //   const link = $(ele).attr("href");
-  //   console.log(link);
-  //   god.link = link;
-  // });
-  // console.log(god.link);
 };
 
 export default webScraper;
