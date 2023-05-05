89e60c25425998cf20cb0c4a9bbcea1375da5a94
Brian Guterl <brianguterl99@gmail.com>
2023-05-04 22:11:52 -0400

    added catagories for webscraper and created newsfetch function for scraping

---
+++ src/api/newsFetch.ts
@@ -0,0 +1,7 @@
+import webScraper from "src/utils/webScraper";
+
+const newsFetch = async () => {
+  webScraper();
+};
+
+export default newsFetch;
--- src/utils/webScraper.ts
+++ src/utils/webScraper.ts
@@ -5,23 +5,50 @@ const webScraper = async () => {
 
   console.log(`scraping ${url}`);
   // { headless: false } use in launch to see browser for testing
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
   try {
     const page = await browser.newPage();
     await page.goto(url);
     await page.waitForSelector(".home-tile");
-    // await page.waitForSelector('.headline')
-    // await page.waitForSelector('.subhead-right')
+
     let godTiles = await page.evaluate(() => {
       let tileElements = document.body.querySelectorAll(".posts-wrapper a");
-      let godTiles = Object.values(tileElements).map((tile) => {
-        // if ()
-        return {
-          tileUrl: tile.getAttribute("href"),
-          headLine: tile.querySelector(".headline").textContent ?? null,
-          // subhead: tile.querySelector(".subhead-right div").contains("div")
-        };
-      });
+      let godTiles = Object.values(tileElements)
+        .map((tile) => {
+          const subhead =
+            tile.querySelector(".subhead.right").textContent ?? null;
+          if (
+            subhead === "Console,Dev Insights" &&
+            tile.querySelector(".headline").textContent.includes("Bonus")
+          ) {
+            return {
+              type: "minorPatchInfo",
+              articleUrl: tile.getAttribute("href"),
+              headLine: tile.querySelector(".headline").textContent,
+            };
+          } else if (subhead === "Console,Dev Insights") {
+            return {
+              type: "majorPatchInfo",
+              articleUrl: tile.getAttribute("href"),
+              headLine: tile.querySelector(".headline").textContent,
+            };
+          } else if (subhead === "Dev Insights,Gods") {
+            return {
+              type: "godInfo",
+              articleUrl: tile.getAttribute("href"),
+              headLine: tile.querySelector(".headline").textContent,
+            };
+          } else if (subhead === "Dev Insights,News") {
+            return {
+              type: "seasonInfo",
+              articleUrl: tile.getAttribute("href"),
+              headLine: tile.querySelector(".headline").textContent,
+            };
+          } else {
+            return;
+          }
+        })
+        .filter((tile) => tile);
       return godTiles;
     });
     await browser.close();
