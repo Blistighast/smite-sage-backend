bbd38274c01c446370b467e5acf0dacc104ef0db
Brian Guterl <brianguterl99@gmail.com>
2023-10-24 20:48:38 -0400

    swapped puppeteer core back to puppeteer and changed puppeteer settings, changed timout to 30 seconds for serverless requests

--- src/utils/webScraper.ts
+++ src/utils/webScraper.ts
@@ -1,5 +1,5 @@
-// import puppeteer from "puppeteer";
-import puppeteer from "puppeteer-core";
+import puppeteer from "puppeteer";
+// import puppeteer from "puppeteer-core";
 import chromium from "@sparticuz/chromium";
 
 import getDateFromTimeAgo from "./getDateFromTimeAgo";
@@ -15,29 +15,29 @@ const webScraper = async () => {
 
   retry++;
 
-  // let proxyList = [
-  //   "202.131.234.142:39330",
-  //   "45.235.216.112:8080",
-  //   "129.146.249.135:80",
-  //   "148.251.20.79",
-  // ];
-
-  // var proxy = proxyList[Math.floor(Math.random() * proxyList.length)];
-
   //OLD
   // { headless: false } use in launch to see browser for testing
   // { headless: "new" } change to new headless and stops warning
   // const browser = await puppeteer.launch({
   //   headless: "new",
-  //   // args: ["--proxy-server=" + proxy],
   // });
 
   const browser = await puppeteer.launch({
-    args: process.env.IS_LOCAL ? puppeteer.defaultArgs() : chromium.args,
+    // args: puppeteer.defaultArgs(),
+    args: chromium.args,
+    // args: process.env.IS_LOCAL ? puppeteer.defaultArgs() : chromium.args,
     defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath(),
+    // executablePath:
+    //   // await chromium.executablePath(),
+    //   "/tmp/localChromium/chromium/win64-1213059/chrome-win/chrome",
+
+    // executablePath: process.env.IS_LOCAL
+    //   ? "/tmp/localChromium/chromium/win64-1213798/chrome-win/chrome.exe"
+    //   : await chromium.executablePath(), //use local chrome install in dev
+    // headless: false,
     headless: chromium.headless,
   });
+  console.log("launched puppeteer");
 
   try {
     const page = await browser.newPage();
@@ -117,12 +117,13 @@ const webScraper = async () => {
           datePosted: getDateFromTimeAgo(article.datePosted),
           imageUrl: articleImage,
         });
+        await page.close();
       } catch (err) {
         console.error(err);
       }
     }
-
     await browser.close();
+    console.log("scrape done");
     return articles;
   } catch (err) {
     console.error(err);
